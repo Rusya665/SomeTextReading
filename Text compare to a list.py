a074823f7cfda1c5d1d9170bcfbd82b9b57cdbf3
Rusya665 <Rusya665@gmail.com>
2022-09-28 15:51:35 +0300

    Initial commit

---
+++ Text compare to a list.py
@@ -0,0 +1,23 @@
+import re
+
+
+path_text = r'C:\Users\runiza\OneDrive - O365 Turun yliopisto\Desktop/Text.txt'
+path_AWL = r'C:\Users\runiza\OneDrive - O365 Turun yliopisto\Desktop/AWL.txt'
+AWL_list = []
+counter = 0
+word_counter = 0
+
+
+with open(path_text, encoding="utf8") as my_file:
+    with open(path_AWL, encoding="utf8") as txt_AWL:
+        for lines in txt_AWL:
+            for case in re.findall("\w+", lines.lower()):
+                AWL_list.append(case)
+        for line in my_file:
+            for word in re.findall("\w+", line.lower()):
+                word_counter += 1
+                if word in AWL_list:
+                    print(word)
+                    counter += 1
+
+print(f'In {word_counter} we have {counter} AW')
